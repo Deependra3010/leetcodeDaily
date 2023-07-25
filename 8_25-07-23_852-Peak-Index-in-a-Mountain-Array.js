cd79cb849d5525d2559abd01c9056dcab1205f1b
Deependra Solanki <deepsolanki3010@gmail.com>
2023-07-25 21:04:26 +0530

    Peak Index in a Mountain Array

---
+++ 8_25-07-23_852-Peak-Index-in-a-Mountain-Array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start <= end) {
+        let mid = parseInt(start + (end - start) / 2);
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        } else if (arr[mid] < arr[mid - 1]) {
+            end = mid;
+        } else {
+            start = mid;
+        }
+    }
+};
